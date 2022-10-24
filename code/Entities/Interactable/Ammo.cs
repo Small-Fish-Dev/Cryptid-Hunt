6eea1f29cc7d75d0d8545bbb9f9706cae995cb67
yuberee <59583743+yuberee@users.noreply.github.com>
2022-10-24 22:25:42 +0200

    Items (WIP)

---
+++ code/Entities/Interactable/Ammo.cs
@@ -0,0 +1,21 @@
+﻿namespace SpookyJam2022;
+
+[HammerEntity]
+[EditorModel( "models/placeholders/placeholder_ammo.vmdl" )]
+[Display( Name = "Ammo Box", GroupName = "Items", Description = "Ammo Box for shotgun" )]
+public partial class Ammo : BaseInteractable
+{
+
+	public override string ModelPath => "models/placeholders/placeholder_ammo.vmdl";
+	public override string UseDescription => "Take Ammo";
+	public override Vector3 PromptOffset3D => new Vector3( 0f );
+	public override Vector2 PromptOffset2D => new Vector2( 20f, 30f );
+
+	public override void Interact( Player player )
+	{
+
+
+	}
+
+
+}
