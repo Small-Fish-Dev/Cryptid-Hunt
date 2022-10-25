97b439377c85c88d0cbdc0174218a2e3acfa2a76
rndtrash <6745157+rndtrash@users.noreply.github.com>
2022-10-26 01:57:13 +0300

    Ladders - work, antichrist - hated, yup, its jamming time

--- code/Entities/Player/PlayerController.cs
+++ code/Entities/Player/PlayerController.cs
@@ -10,37 +10,100 @@ public partial class PlayerController : PawnController
 
 	TimeSince lastStep = 0f;
 
+	private bool isTouchingLadder = false;
+	private Vector3 ladderNormal = Vector3.Zero;
+
+	private void CheckLadder()
+	{
+		/*
+		var wishVelocity = new Vector3( Input.Forward, Input.Left, 0 );
+		wishVelocity = wishVelocity.Normal * Rotation.FromYaw( Input.Rotation.Yaw() );
+		 */
+		if ( isTouchingLadder )
+		{
+			if (Input.Pressed(InputButton.Jump))
+			{
+				Velocity = ladderNormal * 100.0f;
+				isTouchingLadder = false;
+
+				return;
+
+			}
+			else if (GroundEntity != null)
+			{
+				isTouchingLadder = false;
+
+				return;
+			}
+		}
+
+		var wishVelocity = EyeRotation.Angles().WithPitch( 0 ).Forward;
+		
+		var trace = Trace.Ray( Position, Position + wishVelocity )
+			.Size( Player.CollisionBox.Mins, Player.CollisionBox.Maxs )
+			.WithTag("ladder")
+			.Ignore(Pawn)
+			.Run();
+		isTouchingLadder = trace.Hit;
+		if (isTouchingLadder)
+			ladderNormal = trace.Normal;
+	}
+	
 	public override void Simulate()
 	{
 		EyeRotation = Input.Rotation;
+		
+		CheckLadder();
 
-		var wishVelocity = new Vector3( Input.Forward, Input.Left, 0 );
-		wishVelocity = wishVelocity.Normal
-			* wishVelocity.Length.Clamp( 0, 1 )
-			* Rotation.FromYaw( Input.Rotation.Yaw() )
-			* ( Input.Down( InputButton.Run ) ? SprintSpeed : MoveSpeed );
+		MoveHelper helper;
+		if ( isTouchingLadder )
+		{
+			#region Ladder
+			var wishVelocity = new Vector3( 0, Input.Left, Input.Forward ); // W to ascend, S to descend
+			wishVelocity = wishVelocity.Normal * Rotation.FromYaw( Input.Rotation.Yaw() );
+			var velocity = wishVelocity * 100;
+			float normalDot = velocity.Dot(ladderNormal);
+			var cross = ladderNormal * normalDot;
+			Velocity = (velocity - cross) + (-normalDot * ladderNormal.Cross(Vector3.Up.Cross(ladderNormal).Normal));
 
-		Velocity = Vector3.Lerp( Velocity, wishVelocity, 12f * Time.Delta )
-			.WithZ( Velocity.z );
+			helper = new(Position, Velocity);
+			helper.Trace = helper.Trace.Size(Player.CollisionBox.Mins, Player.CollisionBox.Maxs).Ignore(Pawn);
 
-		if ( GroundEntity == null )
-			Velocity += gravity * Time.Delta;
+			helper.TryMove(Time.Delta);
+
+			Position = helper.Position;
+			Velocity = helper.Velocity;
+			#endregion
+		}
 		else
 		{
-			// jump ? i think
+			#region Movement
+			var wishVelocity = new Vector3( Input.Forward, Input.Left, 0 );
+			wishVelocity = wishVelocity.Normal * Rotation.FromYaw( Input.Rotation.Yaw() );
+			wishVelocity *= (Input.Down( InputButton.Run ) ? SprintSpeed : MoveSpeed);
+			Velocity = Vector3.Lerp( Velocity, wishVelocity, 12f * Time.Delta )
+				.WithZ( Velocity.z );
+
+			if (GroundEntity == null)
+				Velocity += gravity * Time.Delta;
+			else
+			{
+				// jump ? i think
+			}
+
+			helper = new( Position, Velocity ) { MaxStandableAngle = 60f };
+			helper.Trace = helper.Trace.Size( Player.CollisionBox.Mins, Player.CollisionBox.Maxs )
+				.Ignore( Pawn );
+			helper.TryUnstuck();
+			helper.TryMoveWithStep( Time.Delta, stepSize );
+
+			DebugOverlay.ScreenText( $"{Velocity} {helper.Velocity}" );
+			
+			Position = helper.Position;
+			Velocity = helper.Velocity;
+			#endregion
 		}
-
-		var helper = new MoveHelper( Position, Velocity )
-		{
-			MaxStandableAngle = 60f
-		};
-		helper.Trace = helper.Trace.Size( Player.CollisionBox.Mins, Player.CollisionBox.Maxs )
-			.Ignore( Pawn );
-		helper.TryUnstuck();
-		helper.TryMoveWithStep( Time.Delta, stepSize );
-
-		Position = helper.Position;
-		Velocity = helper.Velocity;
+			
 
 		if ( Velocity.z <= stepSize )
 		{
@@ -51,7 +114,7 @@ public partial class PlayerController : PawnController
 			{
 				Position += tr.Distance * Vector3.Down;
 
-				if ( Velocity.z < 0f ) 
+				if ( Velocity.z < 0f )
 					Velocity = Velocity.WithZ( 0f );
 			}
 		}
@@ -69,11 +132,11 @@ public partial class PlayerController : PawnController
 			var surface = trace.Surface;
 			var sound = surface.Sounds.FootLand;
 
-			Sound.FromEntity( sound, Pawn ).SetVolume( ( surface.ResourceName == "wood" ? 3 : 1 ) * Velocity.Length / 80f );
+			Sound.FromEntity( sound, Pawn )
+				.SetVolume( (surface.ResourceName == "wood" ? 3 : 1) * Velocity.Length / 80f );
 			lastStep = 0f;
-
+				
 		}
-
 	}
 
 	public override void FrameSimulate()
