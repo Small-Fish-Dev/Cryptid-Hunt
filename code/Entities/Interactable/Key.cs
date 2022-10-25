bbb9a538decbe3d5b0576b6ae51198d4f4e7d0f4
yuberee <59583743+yuberee@users.noreply.github.com>
2022-10-25 18:07:48 +0200

    New item models

--- code/Entities/Interactable/Key.cs
+++ code/Entities/Interactable/Key.cs
@@ -1,12 +1,12 @@
 ﻿namespace SpookyJam2022;
 
 [HammerEntity]
-[EditorModel( "models/placeholders/placeholder_key.vmdl" )]
+[EditorModel( "models/items/key.vmdl" )]
 [Display( Name = "Key", GroupName = "Items", Description = "To open locked chest" )]
 public partial class Key : BaseInteractable
 {
 
-	public override string ModelPath => "models/placeholders/placeholder_key.vmdl";
+	public override string ModelPath => "models/items/key.vmdl";
 	public override string UseDescription => "Take Key";
 	public override Vector3 PromptOffset3D => new Vector3( 0f );
 	public override Vector2 PromptOffset2D => new Vector2( 20f, 30f );
--- code/Entities/Interactable/Medkit.cs
+++ code/Entities/Interactable/Medkit.cs
@@ -1,12 +1,12 @@
 ﻿namespace SpookyJam2022;
 
 [HammerEntity]
-[EditorModel( "models/placeholders/placeholder_medkit.vmdl" )]
+[EditorModel( "models/items/medkit.vmdl" )]
 [Display( Name = "Medkit", GroupName = "Items", Description = "Heals the player to full hp" )]
 public partial class Medkit : BaseInteractable
 {
 
-	public override string ModelPath => "models/placeholders/placeholder_medkit.vmdl";
+	public override string ModelPath => "models/items/medkit.vmdl";
 	public override string UseDescription => "Take Medkit";
 	public override Vector3 PromptOffset3D => new Vector3( 0f );
 	public override Vector2 PromptOffset2D => new Vector2( 20f, 30f );
