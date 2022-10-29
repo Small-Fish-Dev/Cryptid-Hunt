a85f09c57004866ee0d3b09531ddebcf59a6de6a
yuberee <59583743+yuberee@users.noreply.github.com>
2022-10-29 02:29:21 +0200

    Ammo and Stuck Door model

--- code/Entities/Interactable/Ammo.cs
+++ code/Entities/Interactable/Ammo.cs
@@ -1,12 +1,12 @@
 ﻿namespace SpookyJam2022;
 
 [HammerEntity]
-[EditorModel( "models/placeholders/placeholder_ammo.vmdl" )]
+[EditorModel( "models/items/ammo.vmdl" )]
 [Display( Name = "Ammo Box", GroupName = "Items", Description = "Ammo Box for shotgun" )]
 public partial class Ammo : BaseInteractable
 {
 
-	public override string ModelPath => "models/placeholders/placeholder_ammo.vmdl";
+	public override string ModelPath => "models/items/ammo.vmdl";
 	public override string UseDescription => "Take Ammo";
 	public override Vector3 PromptOffset3D => new Vector3( 0f );
 	public override Vector2 PromptOffset2D => new Vector2( 20f, 30f );
--- code/Entities/Interactable/LockedDoor.cs
+++ code/Entities/Interactable/LockedDoor.cs
@@ -1,12 +1,12 @@
 ﻿namespace SpookyJam2022;
 
 [HammerEntity]
-[EditorModel( "models/placeholders/placeholder_locked_door.vmdl" )]
+[EditorModel( "models/stuck_door.vmdl" )]
 [Display( Name = "Locked Door", GroupName = "Items", Description = "To be opened with crowbar" )]
 public partial class LockedDoor : BaseInteractable
 {
 
-	public override string ModelPath => "models/placeholders/placeholder_locked_door.vmdl";
+	public override string ModelPath => "models/stuck_door.vmdl";
 	public override string UseDescription => "Blocked Door";
 	public override Vector3 PromptOffset3D => new Vector3( 0f );
 	public override Vector2 PromptOffset2D => new Vector2( 20f, 30f );
