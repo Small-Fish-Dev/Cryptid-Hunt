0b941356e9470c6bc60953381710011eb00765c2
ceitine <fluffy00gt@gmail.com>
2022-10-30 05:02:19 +0200

    standard screeneffects

---
+++ code/Game/Game.RenderHooks.cs
@@ -0,0 +1,26 @@
+﻿namespace SpookyJam2022;
+
+public partial class Game
+{
+	public static ScreenEffects ScreenEffects { get; private set; }
+
+	[Event( "GameStart" )]
+	private void initialize()
+	{
+		if ( Host.IsServer ) return;
+
+		ScreenEffects = Map.Camera.FindOrCreateHook<ScreenEffects>();
+		ScreenEffects.Vignette.Smoothness = 1f;
+		ScreenEffects.Vignette.Intensity = 0.72f;
+		ScreenEffects.Vignette.Roundness = 0.1f;
+
+		ScreenEffects.MotionBlur.Scale = 0.05f;
+		ScreenEffects.MotionBlur.Samples = 8;
+
+		ScreenEffects.FilmGrain.Intensity = 0.01f;
+		ScreenEffects.FilmGrain.Response = 0.5f;
+	}
+
+	[Event.Hotload]
+	private void hotload() => initialize();
+}
