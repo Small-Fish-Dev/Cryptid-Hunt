f9a5165da628b1a2f06ebcb7e953580169f1c0c3
yuberee <59583743+yuberee@users.noreply.github.com>
2025-04-23 23:06:39 +0200

    Church light logic

---
+++ code/ChurchLight.cs
@@ -0,0 +1,25 @@
+using Sandbox;
+
+public sealed class ChurchLight : Component
+{
+	[Property]
+	public List<SpotLight> Lights { get; set; }
+
+	[Property]
+	public Color Color { get; set; }
+
+	protected override void OnFixedUpdate()
+	{
+		var camera = Scene.Camera;
+		if ( !camera.IsValid() ) return;
+
+		foreach ( var light in Lights )
+		{
+			var distance = light.WorldPosition.Distance( camera.WorldPosition );
+			var intensity = MathX.Remap( distance, 1500f, 5000f, 0f, 1f );
+
+			light.LightColor = Color.Black.LerpTo( Color, intensity );
+			Log.Info( intensity );
+		}
+	}
+}
