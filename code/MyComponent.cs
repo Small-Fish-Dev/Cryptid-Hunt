b08248ea47f8bc93048853a2f61077e393cfb99b
yuberee <59583743+yuberee@users.noreply.github.com>
2025-04-24 22:11:02 +0200

    Properly set up the project for crying out loud

--- code/MyComponent.cs
+++ code/MyComponent.cs
@@ -1,44 +1,9 @@
 
-using System;
-using System.Text.Json.Serialization;
-
 public sealed class MyComponent : Component
 {
-	[Property]
-	[JsonIgnore]
-	[Category( "Debug" )]
-	[Title( "Name to Replace" )]
-	string replaceGameObjectsName { get; set; }
+	[Property] public string StringProperty { get; set; }
 
-	[Property]
-	[JsonIgnore]
-	[Category( "Debug" )]
-	[Title( "Prefab to Place" )]
-	GameObject replaceWithPrefab { get; set; }
-
-	[Button( "Find and Replace", "find_replace" ), Category( "Debug" )]
-	public void DebugReplace()
+	protected override void OnUpdate()
 	{
-		if ( string.IsNullOrEmpty( replaceGameObjectsName ) || string.IsNullOrWhiteSpace( replaceGameObjectsName ) ) return;
-		if ( !replaceWithPrefab.IsValid() ) return;
-
-		var foundObjects = Scene.GetAllObjects( true )
-			.Where( x => x.Name.Contains( replaceGameObjectsName, StringComparison.InvariantCultureIgnoreCase ) )
-			.ToList();
-
-		using ( Scene.Push() )
-		{
-			foreach ( var toReplace in foundObjects )
-			{
-				var spawnedPrefab = replaceWithPrefab.Clone();
-				Log.Info( $"Replacing {toReplace} with {spawnedPrefab}." );
-
-				spawnedPrefab.Transform.World = toReplace.Transform.World;
-				spawnedPrefab.SetParent( toReplace.Parent );
-				toReplace.Destroy();
-			}
-		}
-
-		Log.Info( $"Replaced {foundObjects.Count()} GameObjects." );
 	}
 }
