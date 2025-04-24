f0ee672b4d8266f186a5325637b66517adca88b9
yuberee <59583743+yuberee@users.noreply.github.com>
2025-04-24 16:21:51 +0200

    Flickerer component, church lighting

---
+++ code/Flickerer.cs
@@ -0,0 +1,23 @@
+using Sandbox;
+using Sandbox.Utility;
+using System;
+
+public sealed class Flickerer : Component
+{
+	[Property]
+	public List<Light> Lights { get; set; }
+
+	[Property]
+	public Color Color { get; set; }
+
+
+	protected override void OnFixedUpdate()
+	{
+		foreach ( var light in Lights )
+		{
+			float noise = Noise.Perlin( Time.Now * 100f, 0f );
+
+			light.LightColor = Color.Black.LerpTo( Color, noise );
+		}
+	}
+}
