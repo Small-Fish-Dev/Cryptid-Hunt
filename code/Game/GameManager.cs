4fe3f438ca37af79e749b2bd25f93d393c20cb2e
yuberee <59583743+yuberee@users.noreply.github.com>
2025-04-27 17:46:10 +0200

    GameManager

---
+++ code/Game/GameManager.cs
@@ -0,0 +1,11 @@
+﻿namespace CryptidHunt;
+
+public partial class GameManager : Component
+{
+	public static GameManager Instance { get; private set; }
+
+	protected override void OnStart()
+	{
+		Instance = this;
+	}
+}
