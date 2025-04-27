74bec29fc21e65e500170ce70a8413841995abcc
yuberee <59583743+yuberee@users.noreply.github.com>
2025-04-27 20:14:50 +0200

    SoundCue component

---
+++ code/Components/SoundCue.cs
@@ -0,0 +1,20 @@
+﻿namespace CryptidHunt;
+
+public partial class MySoundCue : Component, Component.ITriggerListener
+{
+	[Property]
+	public SoundEvent Sound { get; set; }
+	public bool Activated { get; set; } = false;
+
+	public MySoundCue() { }
+
+	public void OnTriggerEnter( Collider other )
+	{
+		if ( !Active || Activated ) return;
+		if ( !other.GameObject.Parent.Components.TryGet<Player>( out var player, FindMode.EnabledInSelf ) ) return;
+
+		Activated = true;
+
+		GameObject.PlaySound( Sound, GameObject.WorldPosition );
+	}
+}
