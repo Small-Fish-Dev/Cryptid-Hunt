7894e91cf4e5051925664e4a00f1b1beb26a1871
yuberee <59583743+yuberee@users.noreply.github.com>
2025-04-29 00:50:53 +0200

    Input screen hint

---
+++ code/Components/InputScreenHint.cs
@@ -0,0 +1,27 @@
+﻿namespace CryptidHunt;
+
+public partial class InputScreenHint : Component, Component.ITriggerListener
+{
+
+	[Property]
+	public string Hint { get; set; } = " to not do anything";
+	[Property]
+	[InputAction]
+	public string ButtonHint { get; set; } = "Use";
+
+	public bool Activated { get; set; } = false;
+
+	public InputScreenHint() { }
+
+
+	public void OnTriggerEnter( Collider other )
+	{
+		if ( !Active || Activated ) return;
+		if ( !other.GameObject.Parent.Components.TryGet<Player>( out var player, FindMode.EnabledInSelf ) ) return;
+
+		Activated = true;
+
+		Log.Info( $"You have to press {ButtonHint}{Hint}" );
+	}
+
+}
