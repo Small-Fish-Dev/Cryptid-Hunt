cd81efdf5e70789e50b270d04e3cc971c53d1938
yuberee <59583743+yuberee@users.noreply.github.com>
2025-04-29 00:37:56 +0200

    Dicoverable Area

---
+++ code/Components/DiscoverableArea.cs
@@ -0,0 +1,20 @@
+﻿namespace CryptidHunt;
+
+public partial class DiscoverableArea : Component, Component.ITriggerListener
+{
+	[Property]
+	public string AreaName { get; set; } = "World";
+	public bool Activated { get; set; } = false;
+
+	public DiscoverableArea() { }
+
+	public void OnTriggerEnter( Collider other )
+	{
+		if ( !Active || Activated ) return;
+		if ( !other.GameObject.Parent.Components.TryGet<Player>( out var player, FindMode.EnabledInSelf ) ) return;
+
+		Activated = true;
+
+		Log.Info( $"Welcome to {AreaName}" );
+	}
+}
