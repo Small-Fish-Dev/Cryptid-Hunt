937947b570d2c7381c655e7a184c11ae2fd4a13b
yuberee <59583743+yuberee@users.noreply.github.com>
2025-04-28 22:40:28 +0200

    Player CameraPosition and CameraRotation

--- code/Player/Player.Camera.cs
+++ code/Player/Player.Camera.cs
@@ -1,10 +1,16 @@
-﻿namespace CryptidHunt;
+﻿using Sandbox.Services;
+
+namespace CryptidHunt;
 
 public partial class Player
 {
 	[Property]
 	public Vector3 CameraOffset { get; set; } = new Vector3( 0f, 0f, 64f );
 	public TimeUntil CameraShake { get; set; } = 0f;
+
+	public Vector3 CameraPosition;
+	public Rotation CameraRotation;
+
 	public float ShakeIntensity { get; set; } = 10f;
 	public Vector2 _lastShake;
 
@@ -13,8 +19,6 @@ public partial class Player
 
 	public void SetupCamera()
 	{
-		if ( LockInputs ) return;
-
 		var speed = Controller.Velocity.WithZ( 0f ).Length / 220f;
 
 		if ( Controller.GroundObject.IsValid() )
@@ -33,8 +37,16 @@ public partial class Player
 
 		CameraOffset = _cameraOffset + Camera.WorldRotation.Left * sideOffset + WorldRotation.Up * upOffset;
 
-		Camera.WorldPosition = WorldTransform.PointToWorld( CameraOffset );
-		Camera.WorldRotation = Controller.EyeAngles;
+		if ( !LockInputs )
+		{
+			CameraPosition = WorldTransform.PointToWorld( CameraOffset );
+			CameraRotation = Controller.EyeAngles;
+		}
+		else
+			CameraPosition += Camera.WorldRotation.Left * sideOffset + WorldRotation.Up * upOffset;
+
+		Camera.WorldPosition = CameraPosition;
+		Camera.WorldRotation = CameraRotation;
 	}
 
 	public void AddCameraShake( float duration, float intensity )
