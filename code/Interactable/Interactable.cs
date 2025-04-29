4eab15df7541b10d24d5b1f4f0c45b248c161dec
yuberee <59583743+yuberee@users.noreply.github.com>
2025-04-30 01:25:49 +0200

    Interactable

---
+++ code/Interactable/Interactable.cs
@@ -0,0 +1,44 @@
+using static CryptidHunt.Item;
+
+namespace CryptidHunt;
+
+public class Interactable : Component
+{
+	[Property]
+	public string UseDescription { get; set; } = "Take";
+	[Property]
+	public bool Locked { get; set; } = false;
+	[Property]
+	public Vector3 PromptOffset3D { get; set; }
+	[Property]
+	public Vector2 PromptOffset2D { get; set; }
+
+	[Property]
+	public float Amount { get; set; } = 1;
+	[Property]
+	public ItemResource Item { get; set; }
+	public Item ActiveItem { get; set; }
+
+	public virtual void Interact( Player player )
+	{
+		if ( !player.IsValid() ) return;
+		if ( Item == null ) return;
+
+		//player.Inventory?.Insert( Item, Amount );
+		Sound.Play( "pickup", WorldPosition );
+		Destroy();
+	}
+
+	public virtual void Use( Player player )
+	{
+		if ( !player.IsValid() ) return;
+		//if ( player == null
+		//	|| ActiveItem == null
+		//	|| ActiveItem.Container != player.Inventory ) return;
+
+		//var index = player.Inventory.Items.IndexOf( ActiveItem );
+		player.ShakeIntensity = 10f;
+		player.CameraShake = 0.5f;
+		//player.Inventory.Remove( index );
+	}
+}
