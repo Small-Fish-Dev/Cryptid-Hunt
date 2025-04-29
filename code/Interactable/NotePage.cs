9efcfa59ad6312d561cf348728dbdac41f65801d
yuberee <59583743+yuberee@users.noreply.github.com>
2025-04-30 01:25:53 +0200

    NotePage

---
+++ code/Interactable/NotePage.cs
@@ -0,0 +1,47 @@
+﻿namespace CryptidHunt;
+
+public partial class NotePage : Interactable
+{
+	[Property]
+	[TextArea]
+	public string Text { get; set; } = "Hello World";
+	[Property]
+	public bool BloodyPrint { get; set; } = false;
+
+	public bool IsOpen = false;
+
+	public override void Interact( Player player )
+	{
+		if ( !IsOpen )
+		{
+			GameUI.OpenNote( Text, BloodyPrint );
+
+			IsOpen = true;
+			player.LockInputs = true;
+
+			Sound.Play( "page_open", WorldPosition );
+		}
+		else
+		{
+			GameUI.CloseNote();
+
+			IsOpen = false;
+			player.LockInputs = false;
+
+			/*
+
+			var door = Entity.All.OfType<SceneTransferDoor>().Where( x => x.CheckpoindIDTarget == 1 ).FirstOrDefault(); // The first door haha, don't care
+			door.Unlock();
+
+			if ( Text.Contains( "window" ) ) //hehehehe
+			{
+
+				Event.Run( "BrainReveal" );
+
+			}*/
+
+			Sound.Play( "page_close", WorldPosition );
+		}
+
+	}
+}
