30c3724d9ab1bd6bd3fe4b9c88ac6c0f7cf9154b
yuberee <59583743+yuberee@users.noreply.github.com>
2025-05-02 00:52:44 +0200

    Move beartrap down by trace radius

--- code/Interactable/Items/BearTrap.cs
+++ code/Interactable/Items/BearTrap.cs
@@ -11,10 +11,11 @@ public sealed class BearTrap : Item, Component.ITriggerListener
 		if ( Player.Instance.Holding != this ) return;
 
 		var trace = Player.Instance.InteractTrace;
+		var position = trace.EndPosition - trace.Normal * 5f;
 		var rotation = Rotation.LookAt( trace.Hit ? trace.Normal : Vector3.Up ) * Rotation.FromPitch( 90f );
 		var canPlace = trace.Hit && Vector3.Dot( trace.Normal, Vector3.Up ) > 0.8f;
 
-		DebugOverlay.Model( Model.Model, canPlace ? Color.Green.WithAlpha( 0.7f ) : Color.Red.WithAlpha( 0.7f ), Time.Delta, new Transform( trace.EndPosition, rotation ) );
+		DebugOverlay.Model( Model.Model, canPlace ? Color.Green.WithAlpha( 0.7f ) : Color.Red.WithAlpha( 0.7f ), Time.Delta, new Transform( position, rotation ) );
 	}
 	public override void Attack( Player player )
 	{
@@ -23,7 +24,7 @@ public sealed class BearTrap : Item, Component.ITriggerListener
 		if ( !trace.Hit || Vector3.Dot( trace.Normal, Vector3.Up ) <= 0.8f ) return;
 
 		GameObject.Enabled = true;
-		WorldPosition = trace.EndPosition;
+		WorldPosition = trace.EndPosition - trace.Normal * 5f;
 		WorldRotation = Rotation.LookAt( trace.Normal ) * Rotation.FromPitch( 90f );
 		player.Remove( this );
 		Sound.Play( "beartrap_set", WorldPosition );
