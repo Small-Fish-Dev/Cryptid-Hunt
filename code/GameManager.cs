5aa0fca1102e963da4dd0ac97d099f6b7d814068
yuberee <59583743+yuberee@users.noreply.github.com>
2025-05-02 22:35:52 +0200

    SceneTransferDoor

--- code/GameManager.cs
+++ code/GameManager.cs
@@ -4,6 +4,8 @@ public partial class GameManager : Component
 {
 	public static GameManager Instance { get; private set; }
 
+	public bool ReadInitialNote { get; set; } = false;
+
 	protected override void OnStart()
 	{
 		Instance = this;
---
+++ code/Interactable/SceneTransferDoor.cs
@@ -0,0 +1,39 @@
+﻿namespace CryptidHunt;
+
+public partial class SceneTransferDoor : Interactable
+{
+	[Property]
+	public ModelRenderer Model { get; set; }
+	public override string InteractDescription => Locked ? "LOCKED" : "Open";
+	public override bool Locked => !GameManager.Instance.ReadInitialNote;
+	public bool Open { get; set; } = false;
+	float _degrees = 0f;
+
+	protected override void OnUpdate()
+	{
+		base.OnUpdate();
+
+		if ( !Open ) return;
+
+		WorldRotation *= Rotation.FromYaw( -_degrees * Time.Delta );
+		_degrees = Math.Max( _degrees - Time.Delta * 25f, 0f );
+	}
+
+	public override void Interact( Player player )
+	{
+		if ( !player.IsValid() ) return;
+
+		if ( Open ) return;
+
+		if ( Locked )
+			Sound.Play( "chest_locked", WorldPosition );
+		else
+		{
+			Sound.Play( "door_open", WorldPosition );
+			Open = true;
+			_degrees = 25f;
+		}
+
+		player.AddCameraShake( 0.3f, 8f );
+	}
+}
