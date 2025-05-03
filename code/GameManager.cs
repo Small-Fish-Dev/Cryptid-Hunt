7006e56e5ce49bcdedc24f5e13d543d1571dfd32
yuberee <59583743+yuberee@users.noreply.github.com>
2025-05-03 11:13:54 +0200

    Wind sound manager, inside and outside check

--- code/GameManager.cs
+++ code/GameManager.cs
@@ -1,13 +1,69 @@
-﻿namespace CryptidHunt;
+﻿using System.Diagnostics;
+
+namespace CryptidHunt;
 
 public partial class GameManager : Component
 {
 	public static GameManager Instance { get; private set; }
 
+	[Property]
+	public SoundEvent WindSound { get; set; }
+	SoundHandle _windSoundHandle;
 	public bool ReadInitialNote { get; set; } = false;
 
 	protected override void OnStart()
 	{
 		Instance = this;
+	}
+
+	TimeUntil _nextInsideCheck;
+
+	protected override void OnFixedUpdate()
+	{
+		if ( !_windSoundHandle.IsValid() || !_windSoundHandle.IsPlaying )
+		{
+			_windSoundHandle = Sound.Play( WindSound );
+		}
+		else
+		{
+			if ( !_nextInsideCheck ) return;
+			_nextInsideCheck = 0.5f;
+
+			var player = Player.Instance;
+			if ( player.IsValid() )
+			{
+				var directions = new Vector3[]
+				{
+					Vector3.Zero,
+					Vector3.Forward,
+					Vector3.Backward,
+					Vector3.Left,
+					Vector3.Right
+				};
+
+				var isInside = true;
+
+				foreach ( var direction in directions )
+				{
+					var offset = direction * 100f;
+					var trace = Scene.Trace.Ray( player.WorldPosition + Vector3.Up * 10f, player.WorldPosition + offset + Vector3.Up * 10f )
+						.IgnoreDynamic()
+						.Run();
+
+					var upTrace = Scene.Trace.Ray( trace.EndPosition, trace.EndPosition + Vector3.Up * 1500f )
+						.IgnoreDynamic()
+						.Run();
+
+					if ( !upTrace.Hit )
+					{
+						isInside = false;
+						break;
+					}
+				}
+
+				var targetVolume = isInside ? 1f : 3f;
+				_windSoundHandle.Volume = MathX.Lerp( _windSoundHandle.Volume, targetVolume, Time.Delta * 20f );
+			}
+		}
 	}
 }
