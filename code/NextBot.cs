3bdb195e8fb896b9cd98b5ca3a21664549adc43b
yuberee <59583743+yuberee@users.noreply.github.com>
2025-05-03 17:33:45 +0200

    NextBot sounds

--- code/NextBot.cs
+++ code/NextBot.cs
@@ -15,15 +15,26 @@ public sealed class NextBot : Component
 	[Property]
 	public Computer Computer { get; set; }
 
+	[Property]
+	public SoundEvent ScarySound { get; set; }
 
+
+	TimeUntil _nextSound = 0f;
 	protected override void OnFixedUpdate()
 	{
+		var distance = WorldPosition.Distance( Target.WorldPosition );
 		if ( Computer.IsValid() && Computer.Started )
 		{
 			Agent.MoveTo( Target.WorldPosition );
 
-			if ( WorldPosition.Distance( Target.WorldPosition ) <= 100f )
+			if ( distance <= 70f )
 				Target.Die();
+		}
+
+		if ( _nextSound )
+		{
+			Sound.Play( ScarySound, WorldPosition ).Volume = MathX.Remap( distance, 100f, 1000f, 2f, 0.1f );
+			_nextSound = 1f;
 		}
 	}
 }
